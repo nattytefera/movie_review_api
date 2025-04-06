3a404e0eec3094cca0dc83f752093966df906f02
nattytefera <nhattysafari@gmail.com>
2025-04-06 21:21:49 +0300

    fixing the tmdb for search purpose only

--- movie_api/reviews/serializers.py
+++ movie_api/reviews/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from . import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # class UserSerializer(serializers.ModelSerializer):
 #     class Meta:
--- movie_api/reviews/views.py
+++ movie_api/reviews/views.py
@@ -2,34 +2,34 @@ from rest_framework import viewsets, permissions, generics
 from .models import Movie, Review
 from .serializers import MovieSerializer, ReviewSerializer
 from django.contrib.auth.models import User
-# import requests
+import requests
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 # Create your views here.
 
-# @api_view(['GET'])
-# def tmdb_search(request):
-#     query = request.query_params.get('query', '')
+@api_view(['GET'])
+def tmdb_search(request):
+    query = request.query_params.get('query', '')
     
-#     if not query:
-#         return Response({"error": "Add ?query=YourMovieTitle to the URL"}, status=400)
+    if not query:
+        return Response({"error": "Add ?query=YourMovieTitle to the URL"}, status=400)
     
-#     try:
-#         response = requests.get(
-#             f"{settings.TMDB_BASE_URL}/search/movie",
-#             params={
-#                 "api_key": settings.TMDB_API_KEY,
-#                 "query": query,
-#                 "language": "en-US"
-#             }
-#         )
-#         response.raise_for_status()  # Raises an error for bad status codes
-#         return Response(response.json().get("results", []))
+    try:
+        response = requests.get(
+            f"{settings.TMDB_BASE_URL}/search/movie",
+            params={
+                "api_key": settings.TMDB_API_KEY,
+                "query": query,
+                "language": "en-US"
+            }
+        )
+        response.raise_for_status()  # Raises an error for bad status codes
+        return Response(response.json().get("results", []))
     
-#     except requests.exceptions.RequestException as e:
-#         return Response({"error": f"TMDB request failed: {str(e)}"}, status=500)
+    except requests.exceptions.RequestException as e:
+        return Response({"error": f"TMDB request failed: {str(e)}"}, status=500)
     
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
